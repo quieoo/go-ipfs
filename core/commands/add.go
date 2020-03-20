ipfs/kubo

feat(file-ignore): add ignore opts to add cmd
- add the `gitignore` or `ignore options to the add command
- update go-ipfs-as-a-library example to use new SerialFile constructor

core/commands/add.go
@@ -113,6 +113,8 @@ You can now check what blocks have been created by:
 		cmds.OptionDerefArgs,     // a builtin option that resolves passed in filesystem links (--dereference-args)
 		cmds.OptionStdinName,     // a builtin option that optionally allows wrapping stdin into a named file
 		cmds.OptionHidden,
+		cmds.OptionUseGitignore,
+		cmds.OptionIgnore,
 		cmds.BoolOption(quietOptionName, "q", "Write minimal output."),
 		cmds.BoolOption(quieterOptionName, "Q", "Write only final hash."),
 		cmds.BoolOption(silentOptionName, "Write no output."),

quieoo/go-ipfs

core/commands/add.go
@@ -118,6 +118,8 @@ only-hash, and progress/status related flags) will change the final hash.
 		cmds.OptionDerefArgs,     // a builtin option that resolves passed in filesystem links (--dereference-args)
 		cmds.OptionStdinName,     // a builtin option that optionally allows wrapping stdin into a named file
 		cmds.OptionHidden,
+		cmds.OptionIgnore,
+		cmds.OptionIgnoreRules,
 		cmds.BoolOption(quietOptionName, "q", "Write minimal output."),
 		cmds.BoolOption(quieterOptionName, "Q", "Write only final hash."),
 		cmds.BoolOption(silentOptionName, "Write no output."),